73e40fbd1cad03d74e8cdc3b3ea843d71531f7ca
branquinhoaa <amanda.alves15@gmail.com>
2018-03-12 09:06:07 -0300

    include more code lines and explanations

--- ML_strings.py
+++ ML_strings.py
@@ -2,8 +2,32 @@
 
 from sklearn.feature_extraction.text import CountVectorizer
 
+vectorizer = CountVectorizer()
+
 # Then, we can create or import our emails/texts/phrases to fill our bag of words
 
 email1 = "Good morning, I would like to schedulle a meeting with you."
 email2 = "Ok, we can meet saturday at 11 am, what do you think?"
 
+list_of_emails = [email1, email2]
+
+# Now, we will start to use the  count vectorizer to create a bag of words. 
+# A bag of words is simply a frequency count for all the words used in your text.
+
+# this line below will take each word in list of emails and put in the bag
+bag_of_words = vectorizer.fit(list_of_emails)
+
+# this one will generate all the counts/ words frequency for each word in the bag
+bag_of_words = vectorizer.transform(list_of_emails)
+
+# after this, I will have an output with a list of tuples (indicating the document number, word number) 
+# and a list of integers indicating the word count
+# just like this: (document number - in my case the variable email, word number - the word order inside the text) word frequency
+
+# Data preprocessing: removing the stepwords (not important ones because they are very common in you vocabulary and does not mean a lot)
+import nltk
+
+# you will need to download the nltk (you can do it using command line)
+
+from nltk.corpus import stopwords
+sw = stopwords.words("english")

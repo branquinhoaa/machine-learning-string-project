f57d99d2528c27d6cffd911f5d8884ca2938099f
branquinhoaa <amanda.alves15@gmail.com>
2018-03-12 08:37:20 -0300

    include initial file and readme

---
+++ ML_strings.py
@@ -0,0 +1,9 @@
+# here we import the module from sklearn that is responsible for read the words and create a bag of words
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Then, we can create or import our emails/texts/phrases to fill our bag of words
+
+email1 = "Good morning, I would like to schedulle a meeting with you."
+email2 = "Ok, we can meet saturday at 11 am, what do you think?"
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# machine-learning-string-project is a personal project developed to learn to use ML with strings
